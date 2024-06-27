7bfb94922bbd5624c5810439cc0d7bcf0e0365c2
Kiran Khayamali <kirankhayamali@gmail.com>
2024-06-27 20:02:52 +0545

    Learning variables and conditionals

--- Day 2/script.js
+++ Day 2/script.js
@@ -10,6 +10,59 @@ let guessCount = 1;
 let resetButton;
 
 function checkGuess(){
-    alert ("I am placeholder");
+    const userGuess = Number(guessField.value);
+    if (guessCount === 1){
+        guesses.textContent = "Previous Guesses :";
+    }
+    guesses.textContent = `$(guesses.textContent), $(userGuess)`;
+
+    if (userGuess === numberRandomizer){
+        lastResult.textContent = "Congratulations! You have guess correctly";
+        lastResult.style.backgroundColor = "green";
+        lowOrHigh.textContent = "";
+        setGameOver();
+    }else if(guessCount === 10){
+        lastResult.textContent = "Game Over! You have guessed incorrectly" + `$(numberRandomizer) is the guess number.`;
+        lastResult.style.backgroundColor= "red";
+        lowOrHigh.textContent = "";
+        setGameOver();
+    }else{
+        lastResult.textContent= "Wrong Guess! Try Again";
+        lastResult.style.backgroundColor = "red";
+        if (userGuess<numberRandomizer){
+            lowOrHigh.textContent= "Last Guess was Low!";  
+        }else if (userGuess > numberRandomizer){
+            lowOrHigh.textContent  = "Last Guess was High!";
+        }
+    }
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
 }
-checkGuess();
+
+guessSubmit.addEventListener("click", checkGuess);    
+function setGameOver(){
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton  = document.createElement("button");
+    resetButton.textContent  = "Start new game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame(){
+    guessCount = 1;
+    const resetParas = document.querySelectorAll(".resetParas p");
+    for (const resetPara of resetParas){
+        resetPara.textContent = "";
+    }
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    guessField.focus();
+
+    lastResult.style.backgroundColor = "white";
+    numberRandomizer = Math.floor(Math.random()*100 ) +1;
+
+}
