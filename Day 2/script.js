3a639ffffe74bc74f5a6ee96855a6ab2127c5528
Kiran Khayamali <kirankhayamali@gmail.com>
2024-06-27 20:10:23 +0545

    Learning variables and conditionals

--- Day 2/script.js
+++ Day 2/script.js
@@ -8,6 +8,33 @@ const guessField = document.querySelector(".guessField");
 
 let guessCount = 1;
 let resetButton;
+function resetGame(){
+    guessCount = 1;
+    const resetParas = document.querySelectorAll(".resetParas p");
+    for (const resetPara of resetParas){
+        resetPara.textContent = "";
+    }
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    guessField.focus();
+
+    lastResult.style.backgroundColor = "white";
+    guesses.style.fontSize = "200%";
+    guesses.style.padding = "10px";
+    numberRandomizer = Math.floor(Math.random()*100 ) +1;
+
+}
+
+function setGameOver(){
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton  = document.createElement("button");
+    resetButton.textContent  = "Start new game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
+}
 
 function checkGuess(){
     const userGuess = Number(guessField.value);
@@ -41,30 +68,4 @@ function checkGuess(){
 }
 
 guessSubmit.addEventListener("click", checkGuess);    
-function setGameOver(){
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton  = document.createElement("button");
-    resetButton.textContent  = "Start new game";
-    document.body.append(resetButton);
-    resetButton.addEventListener("click", resetGame);
-}
 
-function resetGame(){
-    guessCount = 1;
-    const resetParas = document.querySelectorAll(".resetParas p");
-    for (const resetPara of resetParas){
-        resetPara.textContent = "";
-    }
-    resetButton.parentNode.removeChild(resetButton);
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = "";
-    guessField.focus();
-
-    lastResult.style.backgroundColor = "white";
-    guesses.style.fontSize = "200%";
-    guesses.style.padding = "10px";
-    numberRandomizer = Math.floor(Math.random()*100 ) +1;
-
-}
