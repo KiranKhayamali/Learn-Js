d433c09f99163e37ece28f7e450b1a4a22603e6c
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-02 20:42:24 +0545

    Learning about operators

--- Day 4/script.js
+++ Day 4/script.js
@@ -19,3 +19,24 @@ headingA.textContent = (`Hello! I am ${myname} And I am ${myage} years old`);
 
 // Objects
 let dog = {name : "Kuro", breed : "Huskey"};
+
+const num1 = 9; //it is an integer but same type as float. ie. number
+const num2 = 3.8; // it is and floating number
+
+const longFloat = 1.766584958675746364;
+const twoDecimal = longFloat.toFixed(2); // Round up longFloat upto two decimal points 
+
+
+const btn = document.querySelector("button");
+const txt = document.querySelector("p");
+
+btn.addEventListener("click", updateBtn);
+function updateBtn (){
+    if (btn.textContent === "Start Machine"){
+        btn.textContent = "Stop Machine";
+        txt.textContent = "The machine has started.";
+    }else{
+        btn.textContent = "Start Machine";
+        txt.textContent = "The machine has stopped.";
+    }
+}
