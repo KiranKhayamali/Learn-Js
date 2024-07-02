a3524e37429c58919d71f741c7c1ddfc76470fe8
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-02 20:02:58 +0545

    Learning about variables

--- Day 4/script.js
+++ Day 4/script.js
@@ -2,7 +2,12 @@ const buttonA = document.querySelector("#button_A");
 const headingA = document.querySelector("#heading_A");
 
 buttonA.onclick = () => {
-    const name = prompt("What is your name ?"); // helps to take input from user in website
-    alert(`Hello ${name}, nice to meet you!`); // shows result in prompt form instead of text in web like textContent do 
+    const name = prompt("What is your name ?"); // helps to take input from user in website on pop up form  
+    alert(`Hello ${name}, nice to meet you!`); // shows result in pop up form instead of text in web like textContent do 
     headingA.textContent = (`Welcome ${name}`);
 };
+
+let myname = "Kiran";
+let myage = 22;
+
+headingA.textContent = (`Hello! I am ${myname} And I am ${myage} years old`);
