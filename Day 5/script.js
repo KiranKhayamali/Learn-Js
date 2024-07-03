81af750191ad39ce0d00de167810618c009200bb
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-03 19:43:05 +0545

    Learning about concatenation in details

--- Day 5/script.js
+++ Day 5/script.js
@@ -16,7 +16,30 @@ const name = "Kiran";
 const greeting = `Welcome Back ${name}!`;
 console.log(greeting);
 
+//concatenations
 const one = "Good";
 const two = "Morning";
-const joined = `${one} ${two} ${name}`;
+const joined = `${one} ${two} ${name}`; //using template literals
 console.log(joined);
+console.log(one + " " + two + " " + name); //concatenating normal strings
+
+const song = "oonko phool";
+const score = 9.5;
+const highestScore = 10;
+const output = `I like the song "${song}". I gave it a score of ${
+  (score / highestScore) * 100
+}%.`;
+console.log(output);
+
+//multi line strings
+const newlineWithBacktik = `One day you finally knew
+what you had to do, and began,`;
+console.log(newlineWithBacktik);
+
+const newline = "One day you finally knew\nwhat you had to do, and began,";
+console.log(newline);
+
+//concatenating number and string 
+const test = "Front ";
+const number = 39;
+console.log(test + number);
