823069a00fa3a68b784520efcfeefaad56b94c60
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-04 18:25:21 +0545

    Learning about arrays

---
+++ Day 6/script.js
@@ -0,0 +1,18 @@
+//arrays
+const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+console.log(shopping);
+
+const sequence = [1, 1, 2, 3, 5, 8, 13];
+
+console.log(shopping.length); // checking length of shopping array
+console.log(shopping[0]); //accessing array
+
+shopping[0] = "rice";
+console.log(shopping);
+
+const random = ["tree", 795, [0, 1, 2]];
+console.log(random[2][2]); // accessing multidimensional arrays ie. 2
+
+const birds = ["Parrot", "Falcon", "Owl"];
+console.log(birds.indexOf("Owl")); //checking index of an item in array ie. 2
+
