a0c57fa071735f9563e730a24aa297534bdea509
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-04 19:27:56 +0545

    Creating searching platform to show 5 latest search history

--- Day 6/script.js
+++ Day 6/script.js
@@ -1,29 +1,29 @@
 //arrays
-const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
-console.log(shopping);
+// const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+// console.log(shopping);
 
-const sequence = [1, 1, 2, 3, 5, 8, 13];
+// const sequence = [1, 1, 2, 3, 5, 8, 13];
 
-console.log(shopping.length); // checking length of shopping array
-console.log(shopping[0]); //accessing array
+// console.log(shopping.length); // checking length of shopping array
+// console.log(shopping[0]); //accessing array
 
-shopping[0] = "rice";
-console.log(shopping);
+// shopping[0] = "rice";
+// console.log(shopping);
 
-const random = ["tree", 795, [0, 1, 2]];
-console.log(random[2][2]); // accessing multidimensional arrays ie. 2
+// const random = ["tree", 795, [0, 1, 2]];
+// console.log(random[2][2]); // accessing multidimensional arrays ie. 2
 
-const birds = ["Parrot", "Falcon", "Owl"];
-console.log(birds.indexOf("Owl")); //checking index of an item in array ie. 2
+// const birds = ["Parrot", "Falcon", "Owl"];
+// console.log(birds.indexOf("Owl")); //checking index of an item in array ie. 2
 
-//adding items in array
-const cities = ["Manchester", "Liverpool"];
-cities.push("Cardiff");
-console.log(cities); // [ "Manchester", "Liverpool", "Cardiff" ]
-cities.push("Bradford", "Brighton");
-console.log(cities);
-cities.unshift("Edinburgh"); // add item in the begining of the array
-console.log(cities);
+// //adding items in array
+// const cities = ["Manchester", "Liverpool"];
+// cities.push("Cardiff");
+// console.log(cities); // [ "Manchester", "Liverpool", "Cardiff" ]
+// cities.push("Bradford", "Brighton");
+// console.log(cities);
+// cities.unshift("Edinburgh"); // add item in the begining of the array
+// console.log(cities);
 
 //removing items in array
 // cities.pop(); // removes last item from array
@@ -35,7 +35,7 @@ console.log(cities);
 // cities.shift(); // removes first item from array
 // console.log(cities)
 
-const index = cities.indexOf("Liverpool");
+// const index = cities.indexOf("Liverpool");
 // if (index !== -1) {
 //   cities.splice(index, 1); // remove liverpool from the cities array
 // }
@@ -47,22 +47,30 @@ const index = cities.indexOf("Liverpool");
 // console.log(cities);
 
 // Accessing items from array
-const birdGroup = ["Parrot", "Falcon", "Owl"];
+// const birdGroup = ["Parrot", "Falcon", "Owl"];
 
-for (const bird of birdGroup) {
-  console.log(bird);
-}
+// for (const bird of birdGroup) {
+//   console.log(bird);
+// }
 
-function double (number){
-    return number * 2;
-}
-const numbers = [1, 3, 7, 9];
-const doubled = numbers.map(double); // all items of numbers array are doubled
-console.log(doubled);
+// function double (number){
+//     return number * 2;
+// }
+// const numbers = [1, 3, 7, 9];
+// const doubled = numbers.map(double); // all items of numbers array are doubled
+// console.log(doubled);
 
-function isEven(number){
-    return number%2 === 0; // items are returnwed only if condtion is satisfied
-}
-const digits = [1,2,3,4,5,6,7,8,9];
-const even = digits.filter(isEven);
-console.log(even);
+// function isEven(number){
+//     return number%2 === 0; // items are returnwed only if condtion is satisfied
+// }
+// const digits = [1,2,3,4,5,6,7,8,9];
+// const even = digits.filter(isEven);
+// console.log(even);
+
+// //converting string into array and vice versa
+// const data = "Manchester,London,Liverpool,Birmingham,Leeds,Carlisle";
+// const cities = data.split(","); // string to array
+// const commaSeparated = cities.join(","); //array to string
+
+// const dogNames = ["Rocket", "Flash", "Bella", "Slugger"];
+// dogNames.toString(); // array to string
---
+++ Day 6/search.js
@@ -0,0 +1,29 @@
+const list = document.querySelector(".output ul"); // for listing the search history in website
+const searchInput = document.querySelector(".output input"); // getting input search data
+const searchButton = document.querySelector(".output button"); // button for search
+
+list.innerHTML = "";
+let myHistory = []; 
+const MAX_HISTORY = 5;
+
+searchButton.onclick = () =>{
+    if (searchInput !== ""){
+        myHistory.unshift(searchInput.value); // using unshift to push item at the begining and searchInput.value to show value
+    // empty the list so that we don't display duplicate entries
+    // the display is regenerated every time a search term is entered.
+        list.innerHTML = "";
+
+        for (const input of myHistory){
+            const listItem = document.createElement("li"); // create a list type element
+            listItem.textContent = input;
+            list.appendChild(listItem);
+        }
+
+        if (myHistory.length >= MAX_HISTORY){
+            myHistory.pop();
+        } 
+
+        searchInput.value = ""; // the textspace in website is clear for new input
+        searchInput.focus(); //focus the textspace
+    }
+}
