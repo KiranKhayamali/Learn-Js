7ba8122d308ac276f6dd7e4b80feb7dfb9e462aa
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-04 18:26:35 +0545

    Learning about adding and removing items in arrays

--- Day 6/script.js
+++ Day 6/script.js
@@ -16,3 +16,33 @@ console.log(random[2][2]); // accessing multidimensional arrays ie. 2
 const birds = ["Parrot", "Falcon", "Owl"];
 console.log(birds.indexOf("Owl")); //checking index of an item in array ie. 2
 
+//adding items in array
+const cities = ["Manchester", "Liverpool"];
+cities.push("Cardiff");
+console.log(cities); // [ "Manchester", "Liverpool", "Cardiff" ]
+cities.push("Bradford", "Brighton");
+console.log(cities);
+cities.unshift("Edinburgh"); // add item in the begining of the array
+console.log(cities);
+
+//removing items in array
+// cities.pop(); // removes last item from array
+// console.log(cities);
+
+// const removedCity = cities.pop();
+// console.log(removedCity);
+
+// cities.shift(); // removes first item from array
+// console.log(cities)
+
+const index = cities.indexOf("Liverpool");
+// if (index !== -1) {
+//   cities.splice(index, 1); // remove liverpool from the cities array
+// }
+// console.log(cities);
+
+// if (index !== -1) {
+//   cities.splice(index, 2); // remove liverpool and following item from cities array
+// }
+// console.log(cities);
+
