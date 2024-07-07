3c357b3e4243fd9a0b15100864f0a5376df0ffc7
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-07 20:04:30 +0545

    building website to change theme using tenerary operator or inline if else

--- Day 9/test.js
+++ Day 9/test.js
@@ -3,4 +3,17 @@
 //   ? "Happy birthday Mrs. Smith — we hope you have a great day!"
 //   : "Good morning Mrs. Smith.";
 
+const select = document.querySelector("select");
+const html = document.querySelector("html");
+document.body.style.padding = "10px";
 
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.addEventListener("change", () =>
+  select.value === "black"
+    ? update("black", "white")
+    : update("white", "black"),
+);
