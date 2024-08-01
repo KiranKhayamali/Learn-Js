b7be70900b4697b6d72e4e8e2179478787298a36
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-01 21:07:02 +0545

    basic of loop

--- Day 11/script.js
+++ Day 11/script.js
@@ -1,30 +1,14 @@
-const btn = document.querySelector("button");
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
-document.addEventListener("DOMContentLoaded", () =>{
-    	canvas.width = document.documentElement.clientWidth;
-        canvas.height = document.documentElement.clientHeight;
-});
-
-function random(number){
-    return Math.floor(Math.random() * number);
+//Loop
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+for (const cat of cats){
+    console.log(cat);
 }
 
-function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i<100 ; i++){
-        ctx.beginPath();
-        ctx.fillStyle = "rgb(0 150 255 / 50%)";
-        ctx.arc(
-            random(canvas.width), //width of bubbles or circles
-            random(canvas.height), //height of bubbles or circle
-            random(50), //number of circle generated
-            0,
-            2 * Math.PI,
-        );
-        ctx.fill();
-    }
-}
+//using map in array
+function toUpper(string) {
+    return string.toUpperCase();
+} 
+const upperCats = cats.map(toUpper);
+console.log(upperCats);
+// [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
 
-btn.addEventListener("click", draw);
