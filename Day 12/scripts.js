9e81e69c6d13e6c4fc413a479784f4ce8b69db0a
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-02 22:41:48 +0545

    indexing in loop

--- Day 12/scripts.js
+++ Day 12/scripts.js
@@ -3,3 +3,19 @@ const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 for (let i = 0; i < cats.length; i++) {
   console.log(cats[i]);
 }
+
+const catsName = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteCats = "My cats are called ";
+
+for (let i = 0; i < cats.length; i++) {
+  if (i === cats.length - 1) {
+    // We are at the end of the array
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+}
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
+
